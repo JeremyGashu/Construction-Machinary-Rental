a638b7d4c7f04651d80cb895efffff46a8a1ef9d
EbsaTeklu <56020971+EbsaTeklu@users.noreply.github.com>
2020-01-10 21:00:41 +0300

    Update Company.go

--- entity/Company.go
+++ entity/Company.go
@@ -2,14 +2,15 @@ package entity
 
 //Company -
 type Company struct {
-	CompanyID int
-	Name      string
-	Email     string
-	PhoneNo   string
-	Address   string
-	Rating    int
-	ImagePath string //the logo picture path of the company
-	Password  string
-	Account   float32
+	CompanyID   int
+	Name        string
+	Email       string
+	PhoneNo     string
+	Address     string
+	Description string
+	Rating      int
+	ImagePath   string //the logo picture path of the company
+	Password    string
+	Account     float32
 	//This are infos that will be used in the system, another infos like password will be saved in db
 }
