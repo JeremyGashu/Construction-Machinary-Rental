2dd7b54b0cf3388321979260629cd2680da67493
znation2010 <56113884+znation2010@users.noreply.github.com>
2020-01-10 20:22:32 +0300

    Add files via upload
    user-service added

---
+++ admin/service/user.go
@@ -0,0 +1,74 @@
+package service
+
+import (
+	"github.com/ermiasgashu/Construction-Machinary-Rental/admin"
+	"github.com/ermiasgashu/Construction-Machinary-Rental/entity"
+)
+
+// UserServiceImpl implements menu.UserService interface
+type UserServiceImpl struct {
+	UserRepo admin.UserRepository
+}
+
+// NewUserServiceImpl will create new UserService object
+func NewUserServiceImpl(CatRepo admin.UserRepository) *UserServiceImpl {
+	return &UserServiceImpl{UserRepo: CatRepo}
+}
+
+// Users ..() returns list of users
+func (cs *UserServiceImpl) Users() ([]entity.User, error) {
+
+	users, err := cs.UserRepo.Users()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+// StoreUser persists new User information
+func (cs *UserServiceImpl) StoreUser(User entity.User) error {
+
+	err := cs.UserRepo.StoreUser(User)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// User returns a User object with a given id
+func (cs *UserServiceImpl) User(id string) (entity.User, error) {
+
+	c, err := cs.UserRepo.User(id)
+
+	if err != nil {
+		return c, err
+	}
+
+	return c, nil
+}
+
+// UpdateUser updates a cateogory with new data
+func (cs *UserServiceImpl) UpdateUser(User entity.User) error {
+
+	err := cs.UserRepo.UpdateUser(User)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteUser delete a User by its id
+func (cs *UserServiceImpl) DeleteUser(id string) error {
+
+	err := cs.UserRepo.DeleteUser(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
