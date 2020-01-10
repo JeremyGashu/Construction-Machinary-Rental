4399dcf1c1b314df41d4051c1e0771cb3bb89a09
EbsaTeklu <56020971+EbsaTeklu@users.noreply.github.com>
2020-01-10 21:05:53 +0300

    Update service.go

--- admin/service.go
+++ admin/service.go
@@ -2,10 +2,38 @@ package admin
 
 import "github.com/ermiasgashu/Construction-Machinary-Rental/entity"
 
+//CompanyService ..
+type CompanyService interface {
+	Companies() ([]entity.Company, error)
+	Company(id int) (entity.Company, error)
+	UpdateCompany(Company entity.Company) error
+	DeleteCompany(id int) error
+	StoreCompany(Company entity.Company) error
+}
+
+//AdminService ..
 type AdminService interface {
 	Admins() ([]entity.Admin, error)
-	Admin(username string) (entity.Admin, error)
-	AddAdmin(admin entity.Admin) error
-	UpdateAdmin(admin entity.Admin) error
-	DeleteAdmin(id string) error
+	Admin(uname string) (entity.Admin, error)
+	UpdateAdmin(Admin entity.Admin) error
+	DeleteAdmin(uname string) error
+	StoreAdmin(Admin entity.Admin) error
+}
+
+//UserService ..
+type UserService interface {
+	Users() ([]entity.User, error)
+	User(uname string) (entity.User, error)
+	UpdateUser(User entity.User) error
+	DeleteUser(uname string) error
+	StoreUser(User entity.User) error
+}
+
+//CommentService ..
+type CommentService interface {
+	Comments() ([]entity.Comment, error)
+	Comment(id int) (entity.Comment, error)
+	UpdateComment(Comment entity.Comment) error
+	DeleteComment(id int) error
+	StoreComment(User entity.Comment) error
 }
